ed70344e79be8f92a8b71dc11ef5b9f85d65ce36
Ismail <cilginsinek58@gmail.com>
2026-05-20 03:00:21 +0300

    feat: build mariadb and change alpine spesific version

---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
+
+# start temporary daemon
+mysqld --user=mysql --datadir=/var/lib/mysql --bootstrap << EOF
+FLUSH PRIVILEGES;
+
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
+FLUSH PRIVILEGES;
+EOF
